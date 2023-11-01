8f2cbe621f2d6656ddae0823786657da470f1201
Jared Malan <jared.malan7@gmail.com>
2023-10-31 23:15:33 -0600

    complete redo of the hole app fixing API calls

--- js/golf-scorecard.js
+++ js/golf-scorecard.js
@@ -1,134 +1,71 @@
-//======= CLASS TO CREATE PLAYERS ========
-class Player {
-    constructor(name, id = getNextId(), scores = []) {
-      this.name = name;
-      this.id = id;
-      this.scores = scores;
-    }
-  }
-
-let players = []
-
+//===== GLOBAL VARIABLE TO THE API LINK THAT CONTAINS THE GOLF COURSES ==========
 let coursesURL = "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json"
 
-function createPlayer(name){
-  let player = new Player(name)
-  players.push(player)
-  return player
+//=== FUNCTION TO FETCH THE COURSES USING THE GLOBAL COURSES VARIABLE ===
+async function myFetch(url) {
+  const response = await fetch(url)
+  return await response.json()
 }
-//======== API INTEGRATION FOR THE COURSES ===========
+//=== FUNCTION TO FETCH THE DATA FROM EACH COURSE AVAILABLE USING THE COURSE ID===
+async function fetchCourseData(courseId) {
+  try {
+    const courseURL = `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${courseId}.json`
 
-async function getAvailableGolfCourses(url) {
-    const response = await fetch(url);
-    return await response.json();
-  }
+    const courseData = await myFetch(courseURL)
 
-async function handleCourseSelectChange(){
-  let courseId = document.getElementById('course-select').value
-
-  if(courseId){
-
-    try {
-      let teeBoxes = await getGolfCourseDetails(courseId);
-      let teeBoxSelect = document.getElementById('tee-box-select')
-
-      teeBoxSelect.innerHTML = ''
-
-      let teeBoxSelectHtml = ''
-      teeBoxes.forEach((teeBox, index) => {
-        teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${teeBox.TotalYards} yards</option>`
-      })
-
-      teeBoxSelect.innerHTML += teeBoxSelectHtml
-
-    } catch (error){
-      console.error('Error fetching tee boxes:', error)
-    }
-  }else{
-    document.getElementById('tee-box-select').innerHTML = ''
+    console.log('Fetched Course Data: ', courseData)
+  } catch (error) {
+    console.error('Error fetching course data: ', error)
   }
 }
 
+//=== FUNCTION TO RENDER THE COURSES AND CREATE THE COURSE SELECTION ===
+async function populateCourseSelect() {
+  try {
+    const courses = await myFetch(coursesURL)
+    const courseOptionsDiv = document.getElementById('course-options')
 
-async function getGolfCourseDetails(golfCourseId) {
-    const response = await fetch(
-          `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${golfCourseId}.json`,
+    // Clear an existing options
+    courseOptionsDiv.innerHTML = ''
 
-      );
-      // console.log();
-      return await response.json();
+    // Create a default option
+    courses.forEach(async (course) => {
+      try {
+        let _course = await myFetch(course.url)
+        console.log("Course Details:", _course)
+
+
+        const courseDiv = document.createElement('div')
+        courseDiv.className = "course-option m-5 text-center text-black"
+
+        const thumbnailImg = document.createElement('img')
+        thumbnailImg.setAttribute('src', _course.thumbnail)
+        thumbnailImg.alt = 'Thumbnail'
+        thumbnailImg.className = "object-cover w-full h-full"
+        courseDiv.appendChild(thumbnailImg)
+
+        const courseName = document.createElement('div')
+        courseName.textContent = _course.name
+        courseName.className = 'courseName p-3 bg-lime'
+        courseDiv.appendChild(courseName)
+
+        courseOptionsDiv.appendChild(courseDiv)
+      } catch (error) {
+        console.error('Error fetching course data:', error)
+      }
+    })
+
+    // This console logs the expected courses in the console
+    // console.log(courses)
+  } catch (error) {
+    console.error('Error fetching and populating golf courses:', error)
   }
-
-  // This is to add an event listener to the course-select element
-document.getElementById('course-options').addEventListener('click', handleCourseSelect)
-
-async function populateCourseSelect(){
-    try {
-      let courses = await getAvailableGolfCourses(coursesURL)
-      let courseOptionsDiv = document.getElementById('course-options')
-
-      // Clear an existing options
-      courseOptionsDiv.innerHTML = ''
-      
-      // Create a default option
-      courses.forEach(async(course) =>{
-      let _course = await getAvailableGolfCourses(course.url)
-    
-      const courseDiv = document.createElement('div')
-      courseDiv.className = "course-option m-5 text-center text-black"
-
-      const thumbnailImg = document.createElement('img')
-      thumbnailImg.setAttribute('src', _course.thumbnail)
-      thumbnailImg.alt = 'Thumbnail'
-      thumbnailImg.className = "object-cover w-full h-full"
-        // console.log(_course.thumbnail)
-      courseDiv.appendChild(thumbnailImg)
-      const courseName = document.createElement('div')
-      courseName.textContent = _course.name
-      courseName.className = 'courseName p-3 bg-lime'
-
-      courseDiv.appendChild(courseName)
-      // courseDiv.appendChild(document.createTextNode(_course.name))
-
-      courseDiv.addEventListener('click', () =>{
-        createPlayerSelection()
-        handleCourseSelect(_course.id)
-      })
-      courseOptionsDiv.appendChild(courseDiv)
-      })
-      
-
-      console.log(courses)
-      // createPlayerSelection() //THIS COULD BE USED TO MAINTAIN IT ACTIVE WHILE STYLING THE PLAYER SELECTION
-    } catch (error){
-        console.error('Error fetching and population golf courses:', error)
-    }   
-} 
+}
 
 function handleCourseSelect(courseId) {
-  if (courseId) {
+  if (courseId) {   
     console.log('selected golf course: ', courseId)
   }
 }
 
 window.addEventListener('load', populateCourseSelect)
-
-
-function createPlayerSelection(){
-  let playerSelection = document.createElement('div')
-  playerSelection.className = 'player-selection text-center flex flex-wrap sm:w-1/2 md:w-1/2 md:ml-auto md:mr-auto justify-center'
-  for (let i = 1; i <=4; i++){
-    let playerOption = document.createElement('div')
-    playerOption.className = 'player-option w-fit bg-lime p-14 m-4 font-bold text-lg'
-    playerOption.textContent = i
-
-    playerOption.addEventListener('click', () =>{
-      let selectedPlayers = i
-      console.log('selected players: ', selectedPlayers)
-    })
-    playerSelection.appendChild(playerOption)
-  }
-
-  let playerSelectionContainer = document.getElementById('player-selection-container')
-  playerSelectionContainer.appendChild(playerSelection)
-}
