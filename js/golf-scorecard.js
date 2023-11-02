85fde510ba618db9df5a2489fee2b1604a327591
Jared Malan <jared.malan7@gmail.com>
2023-11-02 00:29:12 -0600

    player rows on scorecard dynamic creations

--- js/golf-scorecard.js
+++ js/golf-scorecard.js
@@ -8,35 +8,45 @@ async function myFetch(url) {
   return await response.json()
 }
 //=== FUNCTION TO FETCH THE DATA FROM EACH COURSE AVAILABLE USING THE COURSE ID===
+let courseData = []
+let teeBoxData = []
+
 async function fetchCourseData(courseId) {
   try {
     const courseURL = `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${courseId}.json`
 
-    const courseData = await myFetch(courseURL)
+    const fetchedCourseData = await myFetch(courseURL)
 
-   // ==== THIS LOGS THE ENTIRE DATA OF THE COURSE OBJECT ====
-    console.log('Fetched Course Data: ', courseData)
+    // ==== THIS LOGS THE ENTIRE DATA OF THE COURSE OBJECT ====
+    console.log('Fetched Course Data: ', fetchedCourseData)
+
+    // Corrected the assignment of courseData
+    courseData = fetchedCourseData
 
     if (courseData.holes) {
-      courseData.holes.forEach((hole, holeIndex) => {
+      courseData.holes.forEach((hole) => {
         if (hole.teeBoxes) {
-          hole.teeBoxes.forEach((teeBox, teeBoxIndex) => {
-            console.log(`Tee Box ${teeBoxIndex + 1}:`)
-            console.log('Tee Type:', teeBox.teeType)
-            console.log('Par:', teeBox.par)
-            console.log('Yards:', teeBox.yards)
-            console.log('Hcp:', teeBox.hcp)
-            console.log('--------')
+          hole.teeBoxes.forEach((teeBox) => {
+            const teeBoxInfo = {
+              teeBoxId: teeBox.courseHoleTeeBoxId,
+              teeType: teeBox.teeType,
+              par: teeBox.par,
+              yards: teeBox.yards,
+              hcp: teeBox.hcp
+            }
+            teeBoxData.push(teeBoxInfo)
+            console.log('Tee Box Info:', teeBoxInfo)
           })
-          console.log('--------')
         }
       })
+    } else {
+      console.log('No holes found for the selected course')
     }
-
   } catch (error) {
     console.error('Error fetching course data: ', error)
   }
 }
+
 
 //=== FUNCTION TO RENDER THE COURSES AND CREATE THE COURSE SELECTION ===
 async function populateCourseSelect() {
@@ -160,10 +170,12 @@ async function renderTeeBoxes(courseId) {
 
 
 //==== FUNCTION TO CALL & RENDER THE PLAYER OPTIONS AFTER THE TEEBOX WAS SELECTED ====
+let selectedTeeBox = null
+
 function handleTeeBoxSelect(teeBox) {
   console.log('Selected Tee Type:', teeBox.teeType)
   if (teeBox.teeType) {
-    renderPlayerOptions()
+    renderScorecard(courseData, teeBox.teeType)
   }
 }
 
@@ -214,6 +226,7 @@ function renderPlayerOptions() {
 
     playerSelectionContainer.appendChild(playerOption)
   }
+  
 }
 // renderPlayerOptions()
 
@@ -228,5 +241,143 @@ function generatePlayers() {
 
   console.log(`Generated ${numberOfPlayers} players:`)
   console.log(players)
+
+  renderScorecardTable()
 }
 
+//FUNCTION THAT BUILDS THE SCORECARD
+
+function renderScorecardTable() {
+  const scorecardContainer = document.getElementById('scorecard-container')
+  scorecardContainer.innerHTML = '' // Clear any existing content
+
+  const table = document.createElement('table')
+  table.classList.add('table', 'w-full')
+  table.id = 'frontHole'
+
+  // Create the Hole row
+  const holeRow = document.createElement('tr')
+  holeRow.classList.add('text-sm')
+
+  const holeHeader = document.createElement('th')
+  holeHeader.classList.add('p-2', 'font-normal')
+  holeHeader.textContent = 'Hole'
+  holeRow.appendChild(holeHeader)
+
+  for (let i = 1; i <= 18; i++) {
+    const holeColumn = document.createElement('th')
+    holeColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+    holeColumn.textContent = i
+    holeRow.appendChild(holeColumn)
+  }
+
+  // Add the "Total" column
+  const totalColumn = document.createElement('th')
+  totalColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+  totalColumn.textContent = 'Total'
+  holeRow.appendChild(totalColumn)
+
+  table.appendChild(holeRow)
+
+  // Create the Yardage row
+  const yardageRow = document.createElement('tr')
+  yardageRow.classList.add('text-sm', 'bg-charcoal')
+
+  const yardageHeader = document.createElement('th')
+  yardageHeader.classList.add('p-2', 'font-normal', 'bg-charcoal') 
+  yardageHeader.textContent = 'Yardage'
+  yardageRow.appendChild(yardageHeader)
+
+  for (let i = 1; i <= 18; i++) {
+    const yardageColumn = document.createElement('th')
+    yardageColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+    yardageRow.appendChild(yardageColumn)
+  }
+
+   // Add an empty column for the "Total" row
+   const emptyTotalyardageColumn = document.createElement('th')
+   emptyTotalyardageColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+   yardageRow.appendChild(emptyTotalyardageColumn)
+
+  table.appendChild(yardageRow)
+
+  // Create the Par row with the same number of columns as Yardage
+  const parRow = document.createElement('tr')
+  parRow.classList.add('text-sm')
+
+  const parHeader = document.createElement('th')
+  parHeader.classList.add('p-2', 'font-normal')
+  parHeader.textContent = 'Par'
+  parRow.appendChild(parHeader)
+
+  for (let i = 1; i <= 18; i++) {
+    const parColumn = document.createElement('th')
+    parColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+    parRow.appendChild(parColumn)
+  }
+
+  // Add an empty column for the "Total" row
+  const emptyTotalParColumn = document.createElement('th')
+  emptyTotalParColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+  parRow.appendChild(emptyTotalParColumn)
+
+  table.appendChild(parRow)
+
+  // Create the Handicap row with the same number of columns as Yardage
+  const handicapRow = document.createElement('tr')
+  handicapRow.classList.add('text-sm', 'bg-charcoal')
+
+  const handicapHeader = document.createElement('th')
+  handicapHeader.classList.add('p-2', 'font-normal')
+  handicapHeader.textContent = 'Handicap'
+  handicapRow.appendChild(handicapHeader)
+
+  for (let i = 1; i <= 18; i++) {
+    const handicapColumn = document.createElement('th')
+    handicapColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+    handicapRow.appendChild(handicapColumn)
+  }
+
+  // Add an empty column for the "Total" row
+  const emptyTotalHandicapColumn = document.createElement('th')
+  emptyTotalHandicapColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+  handicapRow.appendChild(emptyTotalHandicapColumn)
+
+  table.appendChild(handicapRow)
+
+  // Create player rows based on the number of objects in the players array
+players.forEach((player) => {
+  const playerRow = document.createElement('tr')
+  playerRow.classList.add('text-sm')
+  playerRow.id = `player${player.id}`
+
+  if (player.id === 2 || player.id === 4) {
+    playerRow.classList.add('bg-charcoal');
+  }
+
+  const playerHeader = document.createElement('th')
+  playerHeader.classList.add('p-2', 'font-normal')
+  playerHeader.textContent = player.name
+  playerRow.appendChild(playerHeader)
+
+  for (let j = 1; j <= 18; j++) {
+    const playerColumn = document.createElement('th')
+    playerColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+    playerRow.appendChild(playerColumn)
+  }
+
+  // Add an empty column for the "Total" row
+  const emptyTotalPlayerColumn = document.createElement('th')
+  emptyTotalPlayerColumn.classList.add('p-2', 'font-normal', 'border-l', 'border-l-accents')
+  playerRow.appendChild(emptyTotalPlayerColumn)
+
+  table.appendChild(playerRow)
+})
+
+  scorecardContainer.appendChild(table)
+}
+
+// Call the function to render the scorecard table
+
+
+renderScorecardTable()
